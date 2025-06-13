09cdf072c74129ae83161b95aecb70b894ae6877
Diego Luiz <124363859+dilepego@users.noreply.github.com>
2025-06-13 21:52:52 +0000

    adicionado acesso a api dummyjson

--- todo-app/src/app/tarefas/page.tsx
+++ todo-app/src/app/tarefas/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-import { useState } from "react";
+
+import axios from 'axios'
+import { useEffect, useState } from "react";
 import dados, { TarefaInterface } from "@/data";
 import Cabecalho from "@/componentes/Cabecalho";
 import { ModalTarefa } from "@/componentes/ModalTarefa";
+import { Todo } from "@/app/todos/page"
 
 interface NovaTarefa {
 	titulo: string;
@@ -10,8 +13,42 @@ interface NovaTarefa {
 }
 
 export default function Home() {
-	const [tarefas, setTarefas] = useState<NovaTarefa[]>([]);
+	const [tarefas, setTarefas] = useState<Todo[]>([]);
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
 	const [modalAberto, setModalAberto] = useState(false);
+
+    useEffect(() => {
+    const fetchTarefas = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/todos')
+        setTarefas(response.data.todos)
+      } catch (err) {
+        setError('Failed to fetch todos')
+        console.error(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchTarefas()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-red-500 text-xl">{error}</div>
+      </div>
+    )
+  }
 
 	const adicionarTarefa = (titulo: string) => {
 		setTarefas([...tarefas, { titulo, concluido: false }]);
